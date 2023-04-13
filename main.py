ea0832bf774519cbb7529f070bd0c78c3cea4aaf
AtomSys <zcrenshaw1@icloud.com>
2023-04-13 17:33:34 -0400

    Did More Coding

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 def parse(html):
+    mode = 1
     for letter in html:
-
-    
+        if letter == "<":
+            if mode == 1:
+                mode = 2
+        if letter == ">":
+            if mode == "2":
+                mode = 3
+                    
+            
